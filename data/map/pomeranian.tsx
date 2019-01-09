ba3165299d5f8be533de97875e362d3db91c57a4
Jim Pick <jim@backblend.com>
2019-01-08 19:31:49 -0800

    Add pomeranians

---
+++ data/map/pomeranian.tsx
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="pomeranian" tilewidth="32" tileheight="32" tilecount="96" columns="12">
+ <image source="../img/pomeranian.png" width="384" height="256"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <animation>
+   <frame tileid="12" duration="150"/>
+   <frame tileid="13" duration="150"/>
+   <frame tileid="14" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="24">
+  <animation>
+   <frame tileid="24" duration="150"/>
+   <frame tileid="25" duration="150"/>
+   <frame tileid="26" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="36">
+  <animation>
+   <frame tileid="36" duration="150"/>
+   <frame tileid="37" duration="150"/>
+   <frame tileid="38" duration="150"/>
+  </animation>
+ </tile>
+</tileset>
